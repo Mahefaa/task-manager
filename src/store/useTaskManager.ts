3d0f072b5d9e9613ff4fb50cc2f2c5bc60f4adcf
Mahefaa <hei.mahefa@gmail.com>
2023-06-15 12:28:23 +0300

    chore: implement useTaskManager with localstorage

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,5 +1,6 @@
 import {create} from 'zustand';
 import {Task} from "@/pages/tasks";
+import {TASKS_KEY, useLocalStorage} from "@/hooks/useLocalStorage";
 
 interface TaskManagerState {
     tasks: Task[];
@@ -10,32 +11,62 @@ interface TaskManagerState {
     setSearchTask: (searchTerm: string) => void
 }
 
-const useTaskManager = create<TaskManagerState>((set) => ({
-    tasks: [],
-    searchTask: "",
-    addTask: (task) => {
-        set((state) => ({tasks: [...state.tasks, task]}));
-    },
-
-    updateTask: (taskId, updatedTask) => {
-        set((state) => ({
-            tasks: state.tasks.map((task) =>
-                task.id === taskId ? {...task, ...updatedTask} : task
-            ),
-        }));
-    },
-
-    deleteTask: (taskId) => {
-        set((state) => ({
-            tasks: state.tasks.filter((task) => task.id !== taskId),
-        }));
-    },
-    setSearchTask: searchTerm => {
-        set((state) => ({
-            searchTask: searchTerm
-        }))
+const useTaskManager = create<TaskManagerState>((set) => {
+    let {get, set: setter} = useLocalStorage();
+    const savedTasks = () => {
+        try {
+            const data = get(TASKS_KEY);
+            if (data) {
+                return JSON.parse(data);
+            }
+        } catch (e) {
+            return [];
+        }
     }
-}));
+    console.log("saved Tasks = " + savedTasks())
+    const setSavedTasks = (value: Task[]) => {
+        setter(TASKS_KEY, JSON.stringify(value))
+    }
+
+    return ({
+        tasks: [],
+        searchTask: "",
+        addTask: (task) => {
+            set((state) => {
+                const result = ({tasks: [...state.tasks, task]})
+                setSavedTasks(result.tasks)
+                return result;
+            });
+        },
+
+        updateTask: (taskId, updatedTask) => {
+            set((state) => {
+                const result = ({
+                    tasks: state.tasks.map((task) =>
+                        task.id === taskId ? {...task, ...updatedTask} : task
+                    ),
+                })
+                setSavedTasks(result.tasks)
+                return result
+            });
+        },
+
+        deleteTask: (taskId) => {
+            set((state) => {
+                const result = ({
+                    tasks: state.tasks.filter((task) => task.id !== taskId),
+                })
+                setSavedTasks(result.tasks)
+                return result
+            });
+        },
+        setSearchTask: searchTerm => {
+            set((state) => ({
+                searchTask: searchTerm
+            }))
+        }
+    })
+});
 
 export {
     useTaskManager
