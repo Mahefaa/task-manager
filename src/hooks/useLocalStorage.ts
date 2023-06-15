bcc5373ebbca53100363adb4ee2bb28027eb453b
Mahefaa <hei.mahefa@gmail.com>
2023-06-15 10:21:32 +0300

    chore: implement useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,17 @@
-const useLocalStorage = () => {
+import {Dispatch, SetStateAction, useEffect, useState} from 'react';
 
-}
+export const TASKS_KEY = "tasks";
+
+function useLocalStorage() {
+    const get = (key: string) => localStorage.getItem(key);
+    const set = (key: string, value: string) => localStorage.setItem(key, value);
+    return {
+        get,
+        set
+    }
+};
+
 
 export {
-  useLocalStorage
+    useLocalStorage
 }
